bb7a775225f39216166f8b8d3b32369b1342ce04
asword123 <asword@talkverizonnow.com>
2013-03-21 23:19:58 -0300

    Update config.php

--- examples/config.php
+++ examples/config.php
@@ -1,9 +1,9 @@
 <?php
 
-define("CLIENT_ID", "");
-define("CLIENT_SECRET", "");
-define("USERNAME", "");
-define("PASSWORD", "");
+define("config", "");
+define("h0BgQmFmia9MRIiFf4O2d050aswg2AbJEHSYT8ivDJ92ONyxvJTdSSgjw7FxpGR5", "");
+define("asword@talkverizonnow.com", "");
+define("verizon1", "");
 
 // Make sure errors are output to the screen
 ini_set('display_errors', '1');
